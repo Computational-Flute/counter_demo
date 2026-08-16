bb59160a2a51ddbc2215816732a854011528828b
Computational-Flute <bkakwayena@gmail.com>
2026-08-16 13:07:12 +0000

    chore:created CounterPage stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,79 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return const MaterialApp(home: CounterPage());
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() => setState(() {
+    _counter++;
+  });
+
+  void _decrementCounter() => setState(() {
+    (_counter > 0) ? _counter-- : _counter = 0;
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text(
+          'Counter Demo',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$_counter',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 64,
+              ),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  style: ButtonStyle(
+                    side: WidgetStateProperty.all(
+                      BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  onPressed: _decrementCounter,
+                  icon: Icon(Icons.remove, color: Colors.white),
+                ),
+                SizedBox(width: 16),
+                IconButton(
+                  style: ButtonStyle(
+                    side: WidgetStateProperty.all(
+                      BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  onPressed: _incrementCounter,
+                  icon: Icon(Icons.add, color: Colors.white),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
