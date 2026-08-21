22a1b8ba49e529bed76e1dd0dae67aaff21dce7f
Computational-Flute <bkakwayena@gmail.com>
2026-08-21 10:35:26 +0000

    chore: created tests/ directory and counter_model_test.dart to test counter  bussiness logic

---
+++ test/counter_model_test.dart
@@ -0,0 +1,31 @@
+import 'package:counter_demo/counter_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  //Counter initilizes with 0
+  test('Counter starts at 0', () {
+    final model = CounterModel();
+    expect(model.count, 0);
+  });
+
+  test('Counter increments', () {
+    final model = CounterModel();
+    model.incrementCounter();
+    expect(model.count, 1);
+  });
+
+  test('Counter decrements', () {
+    final model = CounterModel();
+    model.incrementCounter();
+    model.decrementCounter();
+    expect(model.count, 0);
+  });
+
+  test('Counter does not go below zero', () {
+    final model = CounterModel();
+    expect(model.count, 0);
+    model.decrementCounter();
+    model.decrementCounter();
+    expect(model.count, 0);
+  });
+}
